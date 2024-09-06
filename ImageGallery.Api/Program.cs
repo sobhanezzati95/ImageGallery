40fba7daedc4d72d9deeb9854486d14146bce6c8
Asus <Asus@DESKTOP-TBFH4A9>
2024-09-06 20:23:36 +0330

    Add project files.

---
+++ ImageGallery.Api/Program.cs
@@ -0,0 +1,47 @@
+using ImageGallery.API.DbContexts;
+using ImageGallery.API.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("ImageGalleryDBConnectionString");
+builder.Services.AddControllers()
+    .AddJsonOptions(configure => configure.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+builder.Services.AddDbContext<GalleryContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:ImageGalleryDBConnectionString"]);
+});
+
+// register the repository
+builder.Services.AddScoped<IGalleryRepository, GalleryRepository>();
+
+// register AutoMapper-related services
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
