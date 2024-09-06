49d7f35acb74f64aaa710c91e19268e7806a8760
Sobhan_Ezzati <sobhanezzati95@gmail.com>
2024-09-06 20:56:12 +0330

    add IDP project

---
+++ ImageGallery.IDP/Configs.cs
@@ -0,0 +1,47 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace ImageGallery.IDP;
+
+public static class Configs
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            { };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+            {
+                new Client()
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris =
+                    {
+                        "https://localhost:7184/signin-oidc"
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "https://localhost:7184/signout-callback-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    RequireConsent = true
+                }
+            };
+}
